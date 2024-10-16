255f3dfb9c41d5ed42ba409e681bead058d687f2
youss <Yousseflayechi16@gmail.com>
2024-10-16 15:41:23 +0200

    add clacul.py

---
+++ Calcul.py
@@ -0,0 +1,20 @@
+def add(a,b) :
+    return a+b
+
+
+def sous(a,b) :
+    return a-b
+
+
+def mult(a,b) :
+    return a*b
+
+
+def calcul(a,b) :
+    try :
+        return a/b
+    except Exception as e :
+        print(e)
+
+
+
